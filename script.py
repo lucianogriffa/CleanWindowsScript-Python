8b65f034b350fa5ae2a7ea99aa57ed5e99cb4662
Garekox <lucianogriffa@protonmail.com>
2023-01-13 07:55:50 -0300

    test_01

---
+++ script.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.hotkey("win", "r")
+# pyautogui.hotkey("t", "e", "m", "p")
+# pyautogui.hotkey("shift", "5", "t", "e", "m", "p", "shift", "5")
+pyautogui.hotkey("p", "r", "e", "f", "e", "t", "c", "h")
+pyautogui.hotkey("enter", "enter")
+pyautogui.hotkey("tab")
+pyautogui.hotkey("ctrl", "a")
+pyautogui.hotkey("delete")
